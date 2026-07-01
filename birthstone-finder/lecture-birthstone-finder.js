83441542208b039bec9a83cc1f4b0ec5c2ac5fe8
zestypepper10 <meribelmgoldwin@gmail.com>
2026-07-01 13:24:07 -0700

    created things - pizza order printing and birthstone mathcing

--- birthstone-finder/lecture-birthstone-finder.js
+++ birthstone-finder/lecture-birthstone-finder.js
@@ -3,3 +3,25 @@
 ----------------------------
 We will refactor the Birthstone Finder app to read birthstone data from a separate JSON file.
 ------------------------- */
+
+// import fs - file system
+import fs from "fs/promises";
+// const fs = require("fs/promises");
+
+
+// Get user inputted month
+const month = process.argv[2];
+
+// Find the birthstone for the inputted month and print it to the console
+async function getBirthStone() {
+  const data = await fs.readFile("data.json", "utf8");
+  const birthstones = JSON.parse(data);
+
+  // print birthstone with if/else statement
+  if (!birthstones[month]) {
+    console.log("Invalid month. Please enter a valid month name.");
+  } else {
+    console.log(`The birthstone for ${month} is ${birthstones[month]}.`);
+  }
+}
+getBirthStone();
